d72a53451541059a43eed9d51d49963182becc4c
Juan Macías Opinno <juan.macias@opinno.com>
2018-11-06 11:31:53 +0100

    fix(index.jsx): try linter

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,7 @@ import {App} from './App';
 const TEXT_ERROR_LOADING_APP = 'Can\'t find app div for render React. Check index.html';
 
 const loadApp = (rootDiv) => {
-  ReactDOM.render(<App />, rootDiv);
+  ReactDOM.render(<App />, rootDiv)
 }
 const rootDiv = document.getElementById('root');
 if(rootDiv) {
