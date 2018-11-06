e0d40d37ea03a5e881e07cb70206d4df164a1cdf
Juan Macías Opinno <juan.macias@opinno.com>
2018-11-06 12:26:06 +0100

    feat(lint-staged): config lint-staged and run linters in pre-commits

--- src/index.jsx
+++ src/index.jsx
@@ -5,11 +5,11 @@ import {App} from './App';
 const TEXT_ERROR_LOADING_APP = 'Can\'t find app div for render React. Check index.html';
 
 const loadApp = (rootDiv) => {
-  ReactDOM.render(<App />, rootDiv)
-}
+  ReactDOM.render(<App />, rootDiv);
+};
 const rootDiv = document.getElementById('root');
 if(rootDiv) {
-  loadApp(rootDiv)
+  loadApp(rootDiv);
 } else {
-  throw new Error(TEXT_ERROR_LOADING_APP)
+  throw new Error(TEXT_ERROR_LOADING_APP);
 }
