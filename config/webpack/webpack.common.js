40ea21832ef9aaa7fe19c117314432d590e92512
Juan Macías Opinno <juan.macias@opinno.com>
2018-11-06 11:29:12 +0100

    chore(config): add config to webpack and webpack-web-server

--- config/webpack/webpack.common.js
+++ config/webpack/webpack.common.js
@@ -1,13 +1,35 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
 module.exports = {
- module: {
-   rules: [
-     {
-       test: /\.(js|jsx)$/,
-       exclude: /node_modules/,
-       use: {
-         loader: "babel-loader"
-       }
-     }
-   ]
- }
-}
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.html$/,
+        use: {
+          loader: 'html-loader',
+        },
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+      minify: true,
+    }),
+  ],
+};
