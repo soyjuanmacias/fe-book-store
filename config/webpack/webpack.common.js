cb58885fc1d8754937066d408b68b6da6ea1a0f7
Juan Macías Opinno <juan.macias@opinno.com>
2018-11-06 13:42:13 +0100

    chore(webpack): config webpack for css and sass with partial modules

--- config/webpack/webpack.common.js
+++ config/webpack/webpack.common.js
@@ -1,35 +1,60 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ['@babel/preset-env']
+module.exports = (env, argv) => {
+  const devMode = argv.mode && argv.mode === 'development';
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ['@babel/preset-env']
+            }
           }
-        }
-      },
-      {
-        test: /\.html$/,
-        use: {
-          loader: 'html-loader',
         },
-      },
-    ]
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: './src/index.html',
-      filename: './index.html',
-      minify: true,
-    }),
-  ],
+        {
+          test: /\.html$/,
+          use: {
+            loader: 'html-loader',
+          },
+        },
+        {
+          test: /\.(scss|css)$/,
+          exclude: /node_modules/,
+          loaders: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+                sourceMap: true,
+                importLoaders: 1,
+                localIdentName: '[local]'
+              }
+            },
+            'sass-loader',
+          ]
+        },
+      ]
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: './src/index.html',
+        filename: './index.html',
+        minify: true,
+      }),
+      new MiniCssExtractPlugin({
+        filename: devMode ? '[name].css' : '[name].[hash].css',
+        chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+      }),
+    ],
+  }
 };
