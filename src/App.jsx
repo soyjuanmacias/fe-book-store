f3cc3f34a0f18334c6f01d6c6cc390a6a2e608d2
Juan Macías Opinno <juan.macias@opinno.com>
2018-11-11 15:11:15 +0100

    feat(app): add router and home route to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React, {Component} from 'react';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+import {Home} from './components';
 import './App.scss';
 
 export class App extends Component {
   render() {
     return(
-      <div>
-        🌎 🗺️ HOLA MUNDO 🗺️ 🌎
+      <div className="vertical-fill app">
+        <HashRouter>
+          <Switch>
+            <Route path="/" component={Home} />
+          </Switch>
+        </HashRouter>
       </div>
     );
   }
